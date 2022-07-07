ea742a9809b405cceac9ac1c6ec68f81dad244a2
“jtorp” <j.kontakt234@gmail.com>
2022-07-07 10:21:57 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import React from 'react'
+import Spline from '@splinetool/react-spline'
+
+function App() {
+
+  return (
+    <div className='app'>
+      <Spline scene="https://prod.spline.design/7Bxw9DdMUFig9flo/scene.splinecode" />
+    </div>
+  )
+}
+
+export default App

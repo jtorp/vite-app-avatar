add68e400d024d930f8c93f724c9aae60c0f5ff5
Julia Torp <juliatorp@Julias-iMac.webspeed.dk>
2023-02-15 22:32:43 +0100

    update

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,12 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  server:{
+    proxy:{
+      //testing apis stuff
+      "/api/v1": "http://localhost:5000/",
+    }
+  }
+  
 })

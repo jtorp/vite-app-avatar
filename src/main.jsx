d98ca218348aa789e92e7c108dea57502785a35d
Julia Torp <juliatorp@Julias-iMac.webspeed.dk>
2023-02-15 22:30:01 +0100

    update

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,26 @@
 import React from 'react'
 import ReactDOM from 'react-dom '
-import App from './App'
+import { BrowserRouter, Routes, Route} from 'react-router-dom'
 import './index.css'
+import Layout from './components/Layout'
+import Home from './components/Home'
+import CreateNew from './components/CreateNew'
+
+
 
 ReactDOM.render(
+  <> 
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+       <Routes>
+        <Route path='/' element={<Layout/>}>
+        <Route index element={<Home/>}/>
+        <Route path="/create" element={<CreateNew/>}/>
+        </Route>
+        </Routes>
+    </BrowserRouter>
   </React.StrictMode>
+  </>
   ,
   document.getElementById('root')
 )
